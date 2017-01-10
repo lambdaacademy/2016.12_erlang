esl/MongooseIM

Make Elvis happy

apps/ejabberd/src/mod_commands.erl
@@ -186,7 +186,7 @@ send_message(From, To, Body) ->
     ejabberd_hooks:run(user_send_packet,
                        F#jid.lserver,
                        [F, T, Packet]),
-    % privacy check is missing, but is it needed?
+                                                % privacy check is missing, but is it needed?
     ejabberd_router:route(F, T, Packet),
     ok.
 
@@ -242,7 +242,7 @@ lookup_recent_messages(ArcJID, WithJID, Before, Limit) ->
     RSM = #rsm_in{direction = before, id = undefined}, % last msgs
     Start = undefined,
     End = Before * 1000000,
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:system_time(micro_seconds),
     WithJID = WithJID,
     SearchText = undefined,
     PageSize = Limit,

apps/ejabberd/src/mongoose_client_api_messages.erl
@@ -50,7 +50,7 @@ maybe_to_json_with_jid(error, _, Req, State) ->
     Req2 = cowboy_req:reply(404, Req),
     {halt, Req2, State};
 maybe_to_json_with_jid(WithJID, #jid{lserver = Server} = JID, Req, State) ->
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:system_time(micro_seconds),
     ArchiveID = mod_mam:archive_id_int(Server, JID),
     {PageSize, Req2} = maybe_integer_qs_val(cowboy_req:qs_val(<<"limit">>, Req, <<"50">>)),
     {Before, Req3} = maybe_integer_qs_val(cowboy_req:qs_val(<<"before">>, Req2)),

apps/ejabberd/src/mongoose_client_api_rooms_messages.erl
@@ -45,7 +45,7 @@ to_json(Req, #{role_in_room := none} = State) ->
 to_json(Req, #{jid := UserJID, room := Room} = State) ->
     RoomJID = maps:get(jid, Room),
     Server = UserJID#jid.server,
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:system_time(micro_seconds),
     ArchiveID = mod_mam_muc:archive_id_int(Server, RoomJID),
     LimitQSVal = cowboy_req:qs_val(<<"limit">>, Req, <<"50">>),
     {PageSize, Req2} = mongoose_client_api_messages:maybe_integer_qs_val(LimitQSVal),

lambdaacademy/2016.12_erlang

apps/ejabberd/src/mod_commands.erl
@@ -186,7 +186,7 @@ send_message(From, To, Body) ->
     ejabberd_hooks:run(user_send_packet,
                        F#jid.lserver,
                        [F, T, Packet]),
-    % privacy check is missing, but is it needed?
+                                                % privacy check is missing, but is it needed?
     ejabberd_router:route(F, T, Packet),
     ok.
 
@@ -242,7 +242,7 @@ lookup_recent_messages(ArcJID, WithJID, Before, Limit) ->
     RSM = #rsm_in{direction = before, id = undefined}, % last msgs
     Start = undefined,
     End = Before * 1000000,
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:os_system_time(micro_seconds),
     WithJID = WithJID,
     SearchText = undefined,
     PageSize = Limit,

apps/ejabberd/src/mongoose_client_api_messages.erl
@@ -50,7 +50,7 @@ maybe_to_json_with_jid(error, _, Req, State) ->
     Req2 = cowboy_req:reply(404, Req),
     {halt, Req2, State};
 maybe_to_json_with_jid(WithJID, #jid{lserver = Server} = JID, Req, State) ->
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:os_system_time(micro_seconds),
     ArchiveID = mod_mam:archive_id_int(Server, JID),
     {PageSize, Req2} = maybe_integer_qs_val(cowboy_req:qs_val(<<"limit">>, Req, <<"50">>)),
     {Before, Req3} = maybe_integer_qs_val(cowboy_req:qs_val(<<"before">>, Req2)),

apps/ejabberd/src/mongoose_client_api_rooms_messages.erl
@@ -46,7 +46,7 @@ to_json(Req, #{role_in_room := none} = State) ->
 to_json(Req, #{jid := UserJID, room := Room} = State) ->
     RoomJID = maps:get(jid, Room),
     Server = UserJID#jid.server,
-    Now = mod_mam_utils:now_to_microseconds(os:timestamp()),
+    Now = p1_time_compat:os_system_time(micro_seconds),
     ArchiveID = mod_mam_muc:archive_id_int(Server, RoomJID),
     LimitQSVal = cowboy_req:qs_val(<<"limit">>, Req, <<"50">>),
     {PageSize, Req2} = mongoose_client_api_messages:maybe_integer_qs_val(LimitQSVal),